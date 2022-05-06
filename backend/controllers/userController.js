42982b250303b06966e48175f29b292f6fbcae5e
Meyada <79361511+meliy-meyada@users.noreply.github.com>
2022-05-06 18:54:53 +0700

    Create function Connect to database

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,7 +1,9 @@
+const asyncHandler = require('express-async-handler')
+
 // @desc Register a new user
 // @route /api/users
 // @access Public
-const registerUser = (req, res) => {
+const registerUser = asyncHandler(async(req, res) => {
     const { name, email, password } = req.body
     
     // Validation
@@ -11,14 +13,14 @@ const registerUser = (req, res) => {
 
     }
     res.send('Register Route')
-}
+})
 
 // @desc Login a new user
 // @route /api/users/login
 // @access Public
-const loginUser = (req, res) => {
+const loginUser = asyncHandler(async(req, res) => {
     res.send('Login Route')
-}
+})
 
 module.exports = {
     registerUser,
