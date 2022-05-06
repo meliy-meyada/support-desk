71d9b0f6dac07434bdcc20754c4f29cc7f492672
Meyada <79361511+meliy-meyada@users.noreply.github.com>
2022-05-06 17:51:47 +0700

    Design Errorhandle from middleware function response json format

---
+++ backend/controllers/userController.js
@@ -0,0 +1,26 @@
+// @desc Register a new user
+// @route /api/users
+// @access Public
+const registerUser = (req, res) => {
+    const { name, email, password } = req.body
+    
+    // Validation
+    if (!name || !email || !password) {
+        res.status(400)
+        throw new Error('Please include all fields')
+
+    }
+    res.send('Register Route')
+}
+
+// @desc Login a new user
+// @route /api/users/login
+// @access Public
+const loginUser = (req, res) => {
+    res.send('Login Route')
+}
+
+module.exports = {
+    registerUser,
+    loginUser,
+}
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,19 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
+const { errorhandler } = require('./middleware/errorMiddleware')
 const PORT = process.env.PORT || 5000
 
 const app = express()
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
 
 app.get('/', (req, res) => {
-    res.status(200).send({message: 'welcome to the supportdesk'})
+    res.status(200).json({message: 'welcome to the supportdesk API'})
 })
+
+// Routes
+app.use('/api/users', require('./routes/userRoutes'))
+
+app.use(errorhandler)
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
