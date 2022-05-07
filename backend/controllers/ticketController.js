0e4ee9b445d4309221ccaa211bdce3942a8fb7e4
Meyada <79361511+meliy-meyada@users.noreply.github.com>
2022-05-07 18:11:39 +0700

    Create function POST product ticket and protect resource from JWT

--- backend/controllers/ticketController.js
+++ backend/controllers/ticketController.js
@@ -7,19 +7,46 @@ const Ticket = require('../models/ticketModel')
 // @route GET /api/tickets
 // @access Private
 const getTickets = asyncHandler(async (req, res) => {
+    // Get user using th id in the JWT
+    const user = await User.findById(req.user.id)
+
+    if (!user) {
+        res.status(401)
+        throw new Error("User not found")
+    }
+    const tickets = await Ticket.find({user: req.user.id})
     
-    res.status(200).json({message: 'getTicket'})
+    res.status(200).json(tickets)
 })
 // @desc Create new ticket
 // @route POST /api/tickets
 // @access Private
 const createTicket = asyncHandler(async (req, res) => {
+    const { product, description } = req.body
     
-    res.status(200).json({message: 'createTicket'})
+    if (!product || !description) {
+        res.status(400)
+        throw new Error('Please add a product and description')
+    }
+    // Get user using th id in the JWT
+    const user = await User.findById(req.user.id)
+    if (!user) {
+        res.status(401)
+        throw new Error("User not found")
+    }
+
+    const ticket = await Ticket.create({
+        product,
+        description,
+        user: req.user.id,
+        status: 'new'
+    })
+    
+    res.status(201).json(ticket)
 })
 
 
 module.exports = {
     getTickets,
-    createTicket
+    createTicket,
 }
