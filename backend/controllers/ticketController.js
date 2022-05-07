b896bafb72019ccf334ed09c13ab95e28b898387
Meyada <79361511+meliy-meyada@users.noreply.github.com>
2022-05-07 17:40:55 +0700

    Create backend ticket routes ticket controllers

---
+++ backend/controllers/ticketController.js
@@ -0,0 +1,25 @@
+const asyncHandler = require('express-async-handler')
+
+const User = require('../models/userModel')
+const Ticket = require('../models/ticketModel')
+
+// @desc Get user ticket
+// @route GET /api/tickets
+// @access Private
+const getTickets = asyncHandler(async (req, res) => {
+    
+    res.status(200).json({message: 'getTicket'})
+})
+// @desc Create new ticket
+// @route POST /api/tickets
+// @access Private
+const createTicket = asyncHandler(async (req, res) => {
+    
+    res.status(200).json({message: 'createTicket'})
+})
+
+
+module.exports = {
+    getTickets,
+    createTicket
+}
---
+++ backend/routes/ticketRoutes.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const router = express.Router()
+const { getTickets, createTicket} = require('../controllers/ticketController')
+
+
+const {protect} = require('../middleware/authMiddleware') 
+
+router.route('/').get(protect, getTickets).post(protect, createTicket)
+
+
+
+
+module.exports = router
