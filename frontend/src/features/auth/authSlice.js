335f2e1f232492c5b3237635e097fa85eb8f03b5
Meyada <79361511+meliy-meyada@users.noreply.github.com>
2022-05-07 16:19:34 +0700

    Create function register manually return value to mongoose success

--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -1,5 +1,5 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
-
+import authService from './authService'
 
 
 const initialState = {
@@ -13,7 +13,18 @@ const initialState = {
 // Register new User
 export const register = createAsyncThunk('auth/register',
     async (user, thunkAPI) => {
-        console.log(user)
+        try {
+            return await authService.register(user)
+        } catch (error) {
+            const message =
+                (error.response &&
+                    error.response.data &&
+                    error.response.data.message) ||
+            error.message ||
+            error.toString()
+            
+            return thunkAPI.rejectWithValue(message)
+        }
     })
 
 // Login user
@@ -26,9 +37,34 @@ export const login = createAsyncThunk('auth/login',
 export const authSlice = createSlice({
     name: 'auth',
     initialState,
-    reducers: {},
-    extraReducers: (builder) => {},
+    reducers: {
+        reset: (state) => {
+            state.isLoading = false
+            state.isError = false
+            state.isSuccess = false
+            state.message = ''
+        }
+    },
+    extraReducers: (builder) => {
+        builder
+            .addCase(register.pending, (state) => {
+                state.isLoading = true
+            })
+            .addCase(register.fulfilled, (state, action) => {
+                state.isLoading = false
+                state.isSuccess = true
+                state.user = action.payload
+        })
+            .addCase(register.rejected, (state, action) => {
+                state.isLoading = false
+                state.isError = true
+                state.message = action.payload
+                state.user = null
+        })
+    },
 })
 
+
+export const {reset}  = authSlice.actions
 
 export default authSlice.reducer
