5b72197e6cac7446795ab0b44a911dbc044ffc20
Meyada <79361511+meliy-meyada@users.noreply.github.com>
2022-05-08 13:23:07 +0700

    Using redux library create ticketSlice func

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from '../features/auth/authSlice';
+import ticketReducer from '../features/tickets/ticketSlice';
 
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
+    ticket: ticketReducer,
   },
 })
