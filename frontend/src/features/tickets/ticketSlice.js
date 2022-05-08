6f3a22abd5267b6bb1354653dfdbd4b67bf469e1
Meyada <79361511+meliy-meyada@users.noreply.github.com>
2022-05-08 14:01:22 +0700

    Creat Ticket functionality

--- frontend/src/features/tickets/ticketSlice.js
+++ frontend/src/features/tickets/ticketSlice.js
@@ -11,6 +11,25 @@ const initialState = {
   message: ''
 }
 
+// Register new Ticket
+export const createTicket = createAsyncThunk('tickets/create',
+  async (ticketData, thunkAPI) => {
+    try {
+      const token = thunkAPI.getState().auth.user.token
+      return await ticketService.createTicket(ticketData, token)
+    } catch (error) {
+      const message =
+        (error.response &&
+          error.response.data &&
+          error.response.data.message) ||
+        error.message ||
+        error.toString()
+
+      return thunkAPI.rejectWithValue(message)
+    }
+  })
+
+
 export const ticketSlice = createSlice({
   name: 'ticket',
   initialState,
@@ -18,7 +37,19 @@ export const ticketSlice = createSlice({
     reset: (state) => initialState
   },
   extraReducers: (builder) => {
-
+    builder
+      .addCase(createTicket.pending, (state) => {
+        state.isLoading = true
+      })
+      .addCase(createTicket.fulfilled, (state) => {
+        state.isLoading = false
+        state.isSuccess = true
+      })
+      .addCase(createTicket.rejected, (state, action) => {
+        state.isLoading = false
+        state.isError = true
+        state.message = action.payload
+      })
   }
 })
 
--- frontend/src/pages/NewTicket.jsx
+++ frontend/src/pages/NewTicket.jsx
@@ -1,15 +1,46 @@
 import { useState } from "react"
-import { useSelector } from "react-redux"
+import { useSelector, useDispatch } from "react-redux"
+import { useNavigate } from "react-router-dom"
+import { toast } from 'react-toastify'
+import { createTicket, reset } from "../features/tickets/ticketSlice"
+import Spinner from "../components/Spinner"
+import { useEffect } from "react"
+
+
 
 function NewTicket() {
     const { user } = useSelector((state) => state.auth)
+    const {isLoading, isError, isSuccess, message} = useSelector((state) => state.ticket)
+
+
     const [name] = useState(user.name)
     const [email] = useState(user.email)
     const [product, setProduct] = useState('iPhone')
     const [description, setDescription] = useState('')
 
+
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        if (isError) {
+            toast.error(message)
+        }
+        if (isSuccess) {
+            dispatch(reset())
+            navigate('/tickets')
+        }
+        dispatch(reset())
+    }, [dispatch, isError, isSuccess, navigate, message])
+
+
     const onSubmit = (e) => {
         e.preventDefault()
+        dispatch(createTicket({product, description}))
+    }
+
+    if (isLoading) {
+        return<Spinner/>
     }
     
 
